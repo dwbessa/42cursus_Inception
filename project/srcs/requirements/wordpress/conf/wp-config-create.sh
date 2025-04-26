eada2db1425073ca4f3c3a05a58c7ae8d5813d26
dwbessa <vboxuser@inception.myguest.virtualbox.org>
2025-04-26 17:13:29 -0300

    secrets

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -5,6 +5,11 @@ if [ ! -f "/var/www/wp-config.php" ]; then
   /usr/local/bin/wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}" --dbhost="${DB_HOST}" --force
   /usr/local/bin/wp config set FS_METHOD 'direct'
   
+  # Add SSL configuration for admin
+  /usr/local/bin/wp config set FORCE_SSL_ADMIN true --raw
+  /usr/local/bin/wp config set WP_HOME "https://${WP_HOST}"
+  /usr/local/bin/wp config set WP_SITEURL "https://${WP_HOST}"
+  
   /usr/local/bin/wp core install --url="https://${WP_HOST}" --title="${WP_TITLE}" --admin_user="${ADM_WP_NAME}" --admin_password="${ADM_WP_PASS}" --admin_email="${ADM_WP_EMAIL}"
   /usr/local/bin/wp user create "${WP_USERNAME}" "${WP_USEREMAIL}" --role="editor" --user_pass="${WP_USERPASS}"
 fi
